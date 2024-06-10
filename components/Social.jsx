a4ecf955b4ef1b43d5cbef00493f8810527b8d31
ashish7355 <113021007+ashish7355@users.noreply.github.com>
2024-06-10 11:26:23 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: '' },
-    { icon: <FaLinkedinIn />, path: '' },
-    { icon: <FaInstagram />, path: '' },
-    { icon: <FaTwitter />, path: '' },
+    { icon: <FaGithub />, path: 'https://github.com/ashish7355' },
+    { icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/ashish-scott-6a9724215/' },
+    { icon: <FaInstagram />, path: 'https://www.instagram.com/ashish._02/' },
+    { icon: <FaTwitter />, path: 'https://x.com/scott_ashi2202' },
 ]
 
 const Social = ({containerStyles, iconStyles}) => {
